4212521bf5e379b80b403edb83c219f596e7d236
estuar2020 <74992385+estuar2020@users.noreply.github.com>
2021-11-17 19:38:13 -0500

    Ultimo trabajo

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Application.Activities;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -10,20 +12,21 @@ namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
-        public ActivitiesController(DataContext context)
-        {
-            _context = context;
-        }
+         private readonly IMediator _mediator;
+         public ActivitiesController(IMediator mediator)
+         {
+             _mediator = mediator;
+         }
+         [HttpGet]
+         public async Task<ActionResult<List<Activity>>> GetActivities(){
 
-        [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetActivities(){
-            return await _context.Activities.ToListAsync();
-        }
+             return await _mediator.Send(new List.Query());
+         }
 
-        [HttpGet("{Id}")]
-        public async Task<ActionResult<Activity>> GetActivity(Guid Id){
-            return await _context.Activities.FindAsync(Id);
-        }
+          [HttpGet("{id}")]
+         public async Task<ActionResult<Activity>> GetActivity(Guid id){
+
+             return await _mediator.Send(new ListById.QueryId(id));
+         }
     }
 }
